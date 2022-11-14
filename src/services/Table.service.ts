1cf70c0ea455140862ab788d207c234cf2346847
Darren Xu <darrenxu1994@gmail.com>
2022-11-14 20:32:32 +1100

    fetching url

--- src/services/Table.service.ts
+++ src/services/Table.service.ts
@@ -1,5 +1,10 @@
-import { data as tableData } from "../const/data";
-
 export const getData = async () => {
-  return tableData;
+  if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+    const res = await fetch("data.txt").then((response) => response.json());
+    return res.message;
+  }
+  const res = await fetch(
+    `${import.meta.env.PUBLIC_API_URL}${import.meta.env.PUBLIC_END_POINT}`
+  ).then((response) => response.json());
+  return res.message;
 };
